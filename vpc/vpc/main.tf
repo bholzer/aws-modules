5a1d53150e5860dda8d7a2d0727ebf982be98271
Brennan Holzer <bholzer@getwellnetwork.com>
2022-05-07 03:24:39 -0500

    vpc module

---
+++ vpc/vpc/main.tf
@@ -0,0 +1,127 @@
+terraform {
+  required_version = ">= 1.1.9"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 4.12.1"
+    }
+  }
+}
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+locals {
+  availability_zones = slice(data.aws_availability_zones.azs.names, 0, var.zone_count)
+  public_subnets = { for i, cidr in var.public_subnets: cidr => element(local.availability_zones, i) }
+  private_subnets = { for i, cidr in var.private_subnets: cidr => element(local.availability_zones, i) }
+  subnets_by_az = {
+    for az in local.availability_zones: az =>
+      {
+        public = [ for cidr, zone in local.public_subnets: cidr if az == zone ]
+        private = [ for cidr, zone in local.private_subnets: cidr if az == zone ]
+      }
+  }
+  public_nat_subnets = [ for az, obj in local.subnets_by_az: obj.public[0] ]
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+  enable_dns_support = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags = var.tags
+}
+
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
+
+  vpc_id = aws_vpc.this.id
+  availability_zone = each.value
+  cidr_block = each.key
+  map_public_ip_on_launch = true
+  tags = var.tags
+}
+
+resource "aws_subnet" "private" {
+  for_each = local.private_subnets
+
+  vpc_id = aws_vpc.this.id
+  availability_zone = each.value
+  cidr_block = each.key
+  map_public_ip_on_launch = false
+  tags = var.tags
+}
+
+# Public route table
+resource "aws_default_route_table" "public" {
+  default_route_table_id = aws_vpc.this.default_route_table_id
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id = each.value.id
+  route_table_id = aws_default_route_table.public.id
+}
+
+# Private route tables, per-AZ
+resource "aws_route_table" "private" {
+  for_each = toset(local.availability_zones)
+
+  vpc_id = aws_vpc.this.id
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id = each.value.id
+  route_table_id = aws_route_table.private[each.value.availability_zone].id
+}
+
+# Internet routing
+resource "aws_internet_gateway" "this" {
+  count = var.create_internet_gateway ? 1 : 0
+
+  vpc_id = aws_vpc.this.id
+  tags = var.tags
+}
+
+resource "aws_route" "internet" {
+  count = var.create_internet_gateway ? 1 : 0
+
+  route_table_id = aws_default_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this[0].id
+}
+
+resource "aws_eip" "nat" {
+  for_each = var.create_nat ? toset(local.public_nat_subnets) : []
+
+  vpc = true
+  tags = var.tags
+}
+
+resource "aws_nat_gateway" "this" {
+  for_each = var.create_nat ? toset(local.public_nat_subnets) : []
+
+  allocation_id = aws_eip.nat[each.value].id
+  subnet_id = aws_subnet.public[each.value].id
+  tags = var.tags
+}
+
+resource "aws_route" "nat" {
+  for_each = var.create_nat ? aws_route_table.private : {}
+
+  route_table_id = each.value.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = [
+    for az, obj in local.subnets_by_az: [
+      for cidr, nat in aws_nat_gateway.this:
+        nat.id if contains(obj.public, cidr)
+    ][0] if az == each.key
+  ][0]
+}
---
+++ vpc/vpc/variables.tf
@@ -0,0 +1,54 @@
+variable "cidr_block" {
+  type = string
+  description = "The CIDR of the VPC"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  description = "A list of CIDRs to create public subnets for"
+  default = []
+}
+
+variable "private_subnets" {
+  type = list(string)
+  description = "A list of CIDRs to create private subnets for"
+  default = []
+}
+
+variable "zone_count" {
+  type = number
+  description = <<-EOL
+    Number of availability zones to use. Subnets are spread across them evenly.
+    There should be at least as many private and public subnets as this value.
+  EOL
+  default = 1
+}
+
+variable "enable_dns_support" {
+  type = bool
+  description = "Enable DNS support for the VPC"
+  default = true
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+  description = "Enable DNS hostnames for the VPC"
+  default = true
+}
+
+variable "create_internet_gateway" {
+  type = bool
+  description = "Create internet gateway for VPC"
+  default = true
+}
+
+variable "create_nat" {
+  type = bool
+  description = "Create NAT for allowing internet connectivity from private subnets"
+  default = true
+}
+
+variable "tags" {
+  type = map(string)
+  default = {}
+}
